fe63ed88a4a4fd59f32a934db04cbc05d0cb008d
lipeize689-prog <lipeize689@gmail.com>
2026-03-23 21:53:00 +0800

    洛谷上的一道数论问题

---
+++ luogu/P1029 [NOIP 2001 普及组] 最大公约数和最小公倍数问题/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int p, q;
+    cin >> p >> q;
+    
+    if (q % p != 0) {
+        cout << "0\n";
+        return;
+    }
+    
+    int k = q / p;
+    int count = 0;
+    int temp = k;
+    
+    for (int i = 2; i * i <= temp; ++i) {
+        if (temp % i == 0) {
+            count++;
+            while (temp % i == 0) temp /= i;
+        }
+    }
+    if (temp > 1) count++;
+    
+    cout << (1 << count) << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
